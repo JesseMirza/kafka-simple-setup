ed50fc3db01c6fea53d2d8631dfeda4262e0b093
Jesse Mirza <Jesse.Mirza@ahold.com>
2020-03-30 16:32:48 +0200

    Adding 2 extra kafka containers

--- certs/certificates/README.md
+++
@@ -1,4 +0,0 @@
-Certificates in this directory are for testing purpose only.
-You do not need to update them (unless they are expired.)
-
-You can run create-with-docker.sh (.bat) scripts to create test certificates.
--- certs/create-certs.sh
+++ certs/create-certs.sh
@@ -12,6 +12,8 @@ openssl req -new -x509 -keyout ca-key -out ca-cert -days 1825 -subj "/C=NL/ST=NH
 echo "Generate Keystore Files"
 keytool -genkey -noprompt -keystore server1.keystore.jks -alias kafka1 -validity 1825 -keyalg RSA -dname "CN=kafka1, OU=KafkaTeam, O=KafkaNet, L=Amsterdam, S=NH, C=NL" -storepass test1234 -keypass test1234
 keytool -genkey -noprompt -keystore server2.keystore.jks -alias kafka2 -validity 1825 -keyalg RSA -dname "CN=kafka2, OU=KafkaTeam, O=KafkaNet, L=Amsterdam, S=NH, C=NL" -storepass test1234 -keypass test1234
+keytool -genkey -noprompt -keystore server3.keystore.jks -alias kafka3 -validity 1825 -keyalg RSA -dname "CN=kafka3, OU=KafkaTeam, O=KafkaNet, L=Amsterdam, S=NH, C=NL" -storepass test1234 -keypass test1234
+keytool -genkey -noprompt -keystore server4.keystore.jks -alias kafka4 -validity 1825 -keyalg RSA -dname "CN=kafka4, OU=KafkaTeam, O=KafkaNet, L=Amsterdam, S=NH, C=NL" -storepass test1234 -keypass test1234
 keytool -genkey -noprompt -keystore client.keystore.jks -alias client -validity 1825 -keyalg RSA -dname "CN=client, OU=KafkaTeam, O=KafkaNet, L=Amsterdam, S=NH, C=NL" -storepass test1234 -keypass test1234
 keytool -genkey -noprompt -keystore admin.keystore.jks -alias admin -validity 1825 -keyalg RSA -dname "CN=admin, OU=KafkaTeam, O=KafkaNet, L=Amsterdam, S=NH, C=NL" -storepass test1234 -keypass test1234
 
@@ -19,18 +21,24 @@ keytool -genkey -noprompt -keystore admin.keystore.jks -alias admin -validity 18
 echo "Generate Truststore Files which imports CARoot as well (so they trust anyone who has cert signed by this CA)"
 keytool -noprompt -keystore server1.truststore.jks -alias CARoot -import -file ca-cert -storepass test1234
 keytool -noprompt -keystore server2.truststore.jks -alias CARoot -import -file ca-cert -storepass test1234
+keytool -noprompt -keystore server3.truststore.jks -alias CARoot -import -file ca-cert -storepass test1234
+keytool -noprompt -keystore server4.truststore.jks -alias CARoot -import -file ca-cert -storepass test1234
 keytool -noprompt -keystore client.truststore.jks -alias CARoot -import -file ca-cert -storepass test1234
 keytool -noprompt -keystore admin.truststore.jks -alias CARoot -import -file ca-cert -storepass test1234
 
 echo "Create Cert Req"
 keytool -keystore server1.keystore.jks -alias kafka1 -certreq -file cert-file-server-1 -keypass test1234 -storepass test1234
 keytool -keystore server2.keystore.jks -alias kafka2 -certreq -file cert-file-server-2 -keypass test1234 -storepass test1234
+keytool -keystore server3.keystore.jks -alias kafka3 -certreq -file cert-file-server-3 -keypass test1234 -storepass test1234
+keytool -keystore server4.keystore.jks -alias kafka4 -certreq -file cert-file-server-4 -keypass test1234 -storepass test1234
 keytool -keystore client.keystore.jks -alias client -certreq -file cert-file-client -keypass test1234 -storepass test1234
 keytool -keystore admin.keystore.jks -alias admin -certreq -file cert-file-admin -keypass test1234 -storepass test1234
 
 echo "Sign Cert Req with CA Root"
 openssl x509 -req -CA ca-cert -CAkey ca-key -in cert-file-server-1 -out cert-signed-server-1 -days 1825 -CAcreateserial -passin pass:test1234
 openssl x509 -req -CA ca-cert -CAkey ca-key -in cert-file-server-2 -out cert-signed-server-2 -days 1825 -CAcreateserial -passin pass:test1234
+openssl x509 -req -CA ca-cert -CAkey ca-key -in cert-file-server-3 -out cert-signed-server-3 -days 1825 -CAcreateserial -passin pass:test1234
+openssl x509 -req -CA ca-cert -CAkey ca-key -in cert-file-server-4 -out cert-signed-server-4 -days 1825 -CAcreateserial -passin pass:test1234
 openssl x509 -req -CA ca-cert -CAkey ca-key -in cert-file-client -out cert-signed-client -days 1825 -CAcreateserial -passin pass:test1234
 openssl x509 -req -CA ca-cert -CAkey ca-key -in cert-file-admin -out cert-signed-admin -days 1825 -CAcreateserial -passin pass:test1234
 
@@ -42,6 +50,15 @@ keytool -keystore server1.keystore.jks -alias kafka1 -import -file cert-signed-s
 echo "Add CARoot and Signed Request to Server2 keystore"
 keytool -keystore server2.keystore.jks -alias CARoot -import -file ca-cert -storepass test1234 -noprompt
 keytool -keystore server2.keystore.jks -alias kafka2 -import -file cert-signed-server-2 -storepass test1234 -noprompt
+
+echo "Add CARoot and Signed Request to Server2 keystore"
+keytool -keystore server3.keystore.jks -alias CARoot -import -file ca-cert -storepass test1234 -noprompt
+keytool -keystore server3.keystore.jks -alias kafka3 -import -file cert-signed-server-3 -storepass test1234 -noprompt
+
+echo "Add CARoot and Signed Request to Server2 keystore"
+keytool -keystore server4.keystore.jks -alias CARoot -import -file ca-cert -storepass test1234 -noprompt
+keytool -keystore server4.keystore.jks -alias kafka4 -import -file cert-signed-server-4 -storepass test1234 -noprompt
+
 
 echo "Add CARoot and Signed Request to Client keystore"
 keytool -keystore client.keystore.jks -alias CARoot -import -file ca-cert -storepass test1234 -noprompt
